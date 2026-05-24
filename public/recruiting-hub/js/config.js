e768ea6967d4722d95a0af6339d4179efaaf4452
dan <info@bethebestli.com>
2026-05-24 15:47:24 -0400

    Add protected recruiting hub
    Adds the protected BTB parent recruiting hub at /recruiting-hub/.

---
+++ public/recruiting-hub/js/config.js
@@ -0,0 +1,6 @@
+// BTB Recruiting Hub Supabase configuration.
+// This is a publishable browser key protected by Supabase Auth + RLS.
+window.BTB_CONFIG = {
+  SUPABASE_URL: 'https://mckpgoedzubevebhyxhf.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_TBdXrclYShuYkYpJydYlTw__KO_AJV4'
+};
